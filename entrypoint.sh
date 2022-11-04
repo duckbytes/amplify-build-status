8147f76d7bd07520df5fbf2ae3747be49bc2512c
Theo Cranmore <theo.cranmore@gmail.com>
2022-11-04 18:31:51 +0000

    some bash improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,18 @@ BRANCH_NAME=$2
 COMMIT_ID=$3
 WAIT=$4
 TIMEOUT=$5
+export AWS_DEFAULT_REGION="$AWS_REGION"
 
 if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
   echo "You must provide the action with both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables to connect to AWS."
   exit 1
 fi
 
-if [ -z "$AWS_REGION" ] ; then
+if [ -z "$AWS_DEFAULT_REGION" ] ; then
   echo "You must provide AWS_REGION environment variable to connect to AWS."
   exit 1
 fi
 
-export AWS_DEFAULT_REGION="$AWS_REGION"
-export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 
 if [[ $TIMEOUT -lt 0 ]]; then
     echo "Timeout must not be a negative number. Use 0 for unlimited timeout."
@@ -27,25 +25,12 @@ if [[ $TIMEOUT -lt 0 ]]; then
 fi
 
 get_status () {
-    local result;
-    result=$(aws amplify list-jobs --app-id "$1" --branch-name "$2" | jq -r ".jobSummaries[] | select(.commitId == \"$3\") | .status")
-    status=$?
-    echo "$result"
-    return $status
+    local status;
+    status=$(aws amplify list-jobs --app-id "$1" --branch-name "$2" | jq -r ".jobSummaries[] | select(.commitId == \"$3\") | .status")
+    exit_status=$?
+    echo "$status"
+    return $exit_status
 }
-
-check_status () {
-    echo "$1"
-    if [[ "$1" == "SUCCEED" ]]; then
-        return 0
-    elif [[ "$1" == "FAILED" ]]; then
-        return 2
-    else
-        return 1
-    fi
-}
-
-get_status "$APP_ID" "$BRANCH_NAME" "$COMMIT_ID"
 
 STATUS=$(get_status "$APP_ID" "$BRANCH_NAME" "$COMMIT_ID")
 
@@ -54,20 +39,15 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
-check_status "$STATUS"
-
-result=$?
-
-if [[ $result -eq 0 ]]; then
-    echo "Build Succeeded"
+if [[ $STATUS -eq "SUCCEED" ]]; then
+    echo "Build Succeeded!"
     exit 0
-elif [[ $result -eq 2 ]]; then
-    echo "Build Failed"
+elif [[ $STATUS -eq "FAILED" ]]; then
+    echo "Build Failed!"
     exit 1
 fi
 
-echo $?
-echo "Build in progress"
+echo "Build in progress..."
 
 seconds=$(( $TIMEOUT * 60 ))
 count=0
@@ -75,23 +55,22 @@ count=0
 if [[ "$WAIT" == "false" ]]; then
     exit 1
 elif [[ "$WAIT" == "true" ]]; then
-    while [[ $result -ne 0 ]]; do
+    while [[ $STATUS -ne "SUCCEED" ]]; do
         sleep 30
         STATUS=$(get_status "$APP_ID" "$BRANCH_NAME" "$COMMIT_ID")
         if [[ $? -ne 0 ]]; then
             echo "Failed to get status of the job."
             exit 1
         fi
-        check_status "$STATUS"
-        result=$?
-        if [[ result -eq q ]]; then
+        if [[ $STATUS -eq "FAILED" ]]; then
             echo "Build Failed!"
             exit 1
         fi
         count=$(( $count + 30 ))
         if [[ $count -gt $seconds ]] && [[ $TIMEOUT -ne 0 ]] ; then
-            echo "Timed out"
+            echo "Timed out."
             exit 1
         fi
     done
+    echo "Build Succeeded!"
 fi
